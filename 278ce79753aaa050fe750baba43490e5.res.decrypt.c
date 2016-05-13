18c9dc7d8c5867ac7f9baaefb4eff87c5abcba4e
Rix <i@rix.li>
2016-05-13 18:04:40 -0400

    add decrypt.c

---
+++ 278ce79753aaa050fe750baba43490e5.res.decrypt.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    if (argc != 3) {
+        printf("Usage: %s FILEIN FILEOUT\n", argv[0]);
+        exit(1);
+    }
+    FILE *filein = fopen(argv[1], "r");
+    if (!filein) {
+        perror("FILEIN");
+        exit(2);
+    }
+    FILE *fileout = fopen(argv[2], "w");
+    if (!fileout) {
+        perror("FILEOUT");
+        exit(2);
+    }
+    unsigned char c;
+    while (!feof(filein)) {
+        if (!fread(&c, 1, 1, filein)) {
+            break;
+        }
+        c += 0x28;
+        c ^= 0x94;
+        c -= 0x72;
+        if (fwrite(&c, 1, 1, fileout) < 1) {
+            break;
+        }
+    }
+    fclose(filein);
+    fclose(fileout);
+    return 0;
+}
